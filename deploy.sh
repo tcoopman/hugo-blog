1b3850a4bf8cd48b295606e456d9c0f5423d22d6
Thomas Coopman <thomas.coopman@gmail.com>
2021-02-18 10:32:45 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
+
+set -ex
+
 rm -rf public
+mkdir -p public
 hugo --buildDrafts
 chmod -R 755 public
-rsync -avzp -e ssh public/ thomascoopman.eu:/home/thomas/hugo
+docker build . -t eu.gcr.io/infinitetree/hugo
+docker push eu.gcr.io/infinitetree/hugo
+
+ssh infinitetree.eu << EOF
+set -ex
+cd traefik
+docker-compose pull blog
+docker-compose up -d --build blog
+EOF
